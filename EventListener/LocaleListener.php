ff76a49bd689551f8bde137d0652eec13bea921a
Sonja Brodersen <s.brodersen@networking.ch>
2013-01-15 17:44:01 +0100

    set preferred browser locale on first encounter

--- EventListener/LocaleListener.php
+++ EventListener/LocaleListener.php
@@ -17,6 +17,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Http\AccessMapInterface;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @author net working AG <info@networking.ch>
@@ -52,11 +53,17 @@ class LocaleListener implements EventSubscriberInterface
     /**
      * @param \Symfony\Component\Security\Http\AccessMapInterface $accessMap
      * @param \Symfony\Component\Security\Core\SecurityContext $securityContext
+     * @param array $availableLanguages
      * @param string $defaultLocale
      * @param \Symfony\Component\Routing\RouterInterface $router
      */
-    public function __construct(AccessMapInterface $accessMap, SecurityContext $securityContext, array $availableLanguages, $defaultLocale = 'en', RouterInterface $router = null)
-    {
+    public function __construct(
+        AccessMapInterface $accessMap,
+        SecurityContext $securityContext,
+        array $availableLanguages,
+        $defaultLocale = 'en',
+        RouterInterface $router = null
+    ){
         $this->accessMap = $accessMap;
         $this->securityContext = $securityContext;
         $this->availableLanguages = $availableLanguages;
@@ -83,9 +90,13 @@ class LocaleListener implements EventSubscriberInterface
     {
         $request = $event->getRequest();
 
-        if (!$request->getSession()) return;
+        if (!$request->getSession()) {
+            return;
+        }
 
-        if (!$request) return;
+        if (!$request) {
+            return;
+        }
 
         $patterns = $this->accessMap->getPatterns($request);
 
@@ -96,7 +107,9 @@ class LocaleListener implements EventSubscriberInterface
         if ($request->hasPreviousSession()) {
             $request->setDefaultLocale($request->getSession()->get($localeType, $this->defaultLocale));
         } else {
-            $request->setDefaultLocale($this->defaultLocale);
+            $preferredLocale = $this->getPreferredLocale($request);
+            $request->getSession()->set($localeType, $preferredLocale);
+            $request->setDefaultLocale($preferredLocale);
         }
 
 
@@ -118,6 +131,7 @@ class LocaleListener implements EventSubscriberInterface
         }
     }
 
+
     /**
      * @param \Symfony\Component\Security\Http\Event\InteractiveLoginEvent $event
      */
@@ -133,19 +147,135 @@ class LocaleListener implements EventSubscriberInterface
         $frontendLocale = $this->guessFrontendLocale($locale);
 
         $request->getSession()->set('_locale', $frontendLocale);
+    }
 
+    /**
+     * guess frontend locale
+     * @param mixed $locales
+     * @return string
+     */
+    protected function guessFrontendLocale($locales)
+    {
+        // search for match in array
+        if (is_array($locales)) {
+            foreach ($locales as $locale) {
+                $match = $this->matchLocaleInAvailableLanguages($locale);
+                if (strlen($match) > 0) {
+                    return $match;
+                }
+            }
+        } // check if locale matches in available languages
+        else {
+            $match = $this->matchLocaleInAvailableLanguages($locales);
+            if (strlen($match) > 0) {
+                return $match;
+            }
+        }
+
+        return $this->defaultLocale;
     }
 
     /**
      * @param $locale
      * @return string
      */
-    protected function guessFrontendLocale($locale)
+    protected function matchLocaleInAvailableLanguages($locale)
     {
         foreach ($this->availableLanguages as $language) {
-            if (strpos($language['locale'], $locale, 0)) return $language['locale'];
-            if (strpos($language['locale'], substr($locale, 0, 2), 0)) return $language['locale'];
+            if (strpos($language['locale'], $locale, 0)) {
+                return $language['locale'];
+            }
+            if (strpos($language['locale'], substr($locale, 0, 2), 0)) {
+                return $language['locale'];
+            }
         }
-        return $this->defaultLocale;
+
+        return false;
+    }
+
+
+    /**
+     * get preferred locale
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return string
+     */
+    public function getPreferredLocale(Request $request)
+    {
+        $browserAcceptLanguages = $this->getBrowserAcceptLanguages($request);
+        if (empty($browserAcceptLanguages)) {
+            return $this->defaultLocale;
+        }
+
+        return $this->guessFrontendLocale($browserAcceptLanguages);
+    }
+
+    /**
+     * get browser accept languages
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return array
+     */
+    public function getBrowserAcceptLanguages(Request $request)
+    {
+        $browserLanguages = array();
+
+        if (strlen($request->server->get('HTTP_ACCEPT_LANGUAGE')) == 0) {
+            return array();
+        }
+
+        $languages = $this->splitHttpAcceptHeader(
+            $request->server->get('HTTP_ACCEPT_LANGUAGE')
+        );
+        foreach ($languages as $lang) {
+            if (strstr($lang, '-')) {
+                $codes = explode('-', $lang);
+                if ($codes[0] == 'i') {
+                    // Language not listed in ISO 639 that are not variants
+                    // of any listed language, which can be registered with the
+                    // i-prefix, such as i-cherokee
+                    if (count($codes) > 1) {
+                        $lang = $codes[1];
+                    }
+                } else {
+                    for ($i = 0, $max = count($codes); $i < $max; $i++) {
+                        if ($i == 0) {
+                            $lang = strtolower($codes[0]);
+                        } else {
+                            $lang .= '_' . strtoupper($codes[$i]);
+                        }
+                    }
+                }
+            }
+
+            $browserLanguages[] = $lang;
+        }
+
+        return $browserLanguages;
+    }
+
+    /**
+     * split http accept header
+     * @param string $header
+     * @return array
+     */
+    public function splitHttpAcceptHeader($header)
+    {
+        $values = array();
+        foreach (array_filter(explode(',', $header)) as $value) {
+            // Cut off any q-value that might come after a semi-colon
+            if ($pos = strpos($value, ';')) {
+                $q = (float)trim(substr($value, strpos($value, '=') + 1));
+                $value = substr($value, 0, $pos);
+            } else {
+                $q = 1;
+            }
+
+            if (0 < $q) {
+                $values[trim($value)] = $q;
+            }
+        }
+
+        arsort($values);
+
+        return array_keys($values);
     }
 }
