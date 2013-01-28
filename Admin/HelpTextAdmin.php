d74d3366c0b8aa27d6df65ce080b39069cd2767a
Yorkie Chadwick <y.chadwick@networking.ch>
2013-01-28 12:18:56 +0100

    formatting

--- Admin/HelpTextAdmin.php
+++ Admin/HelpTextAdmin.php
@@ -18,103 +18,109 @@ use Sonata\AdminBundle\Form\FormMapper;
 class HelpTextAdmin extends BaseAdmin
 {
     /**
-         * @param \Sonata\AdminBundle\Datagrid\ListMapper $listMapper
-         */
-        protected function configureListFields(ListMapper $listMapper)
-        {
+     * @param \Sonata\AdminBundle\Datagrid\ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
 
-            $listMapper
-                ->addIdentifier('title')
-                ->addIdentifier('translationKey')
-                ->add('locale')
-                ->add(
-                '_action',
-                'actions',
-                array(
-                    'label' => ' ',
-                    'actions' => array(
-                        'edit' => array(),
-                        'delete' => array()
-                    )
+        $listMapper
+            ->addIdentifier('title')
+            ->addIdentifier('translationKey')
+            ->add('locale')
+            ->add(
+            '_action',
+            'actions',
+            array(
+                'label' => ' ',
+                'actions' => array(
+                    'edit' => array(),
+                    'delete' => array()
                 )
-            );
-        }
+            )
+        );
+    }
 
     /**
-         * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
-         */
-        protected function configureFormFields(FormMapper $formMapper)
-        {
-            try {
-                        $request = $this->getRequest();
-                    } catch (\RuntimeException $e) {
-                        $request = $this->container->get('request');
-                    }
+     * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        try {
+            $request = $this->getRequest();
+        } catch (\RuntimeException $e) {
+            $request = $this->container->get('request');
+        }
 
-                    $locale = $request->get('locale') ? $request->get('locale') : $request->getLocale();
+        $locale = $request->get('locale') ? $request->get('locale') : $request->getLocale();
 
-            $formMapper
-                ->add(
-                'locale',
-                'choice',
-                array(
-                    'choices' => $this->getLocaleChoices(),
-
-                    'preferred_choices' => array($locale)
-                ))
-                ->add('translationKey')
-                ->add('title')
-                ->add('text', 'textarea', array('attr' => array(
+        $formMapper
+            ->add(
+            'locale',
+            'choice',
+            array(
+                'choices' => $this->getLocaleChoices(),
+                'preferred_choices' => array($locale)
+            )
+        )
+            ->add('translationKey')
+            ->add('title')
+            ->add(
+            'text',
+            'textarea',
+            array(
+                'attr' => array(
                     'class' => 'tinymce',
-                )));
+                )
+            )
+        );
 
-        }
-
-    /**
-        * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
-        */
-       protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-       {
-           $datagridMapper
-               ->add(
-               'locale',
-               'doctrine_orm_callback',
-               array(
-                   'callback' => array(
-                       $this,
-                       'getByLocale'
-                   )
-               ),
-               'choice',
-               array(
-                   'empty_value' => false,
-                   'choices' => $this->getLocaleChoices(),
-                   'preferred_choices' => array($this->getDefaultLocale())
-               )
-
-           );
-
-       }
+    }
 
     /**
-         * @param \Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQuery $queryBuilder
-         * @param $alias
-         * @param $field
-         * @param $data
-         * @return bool
-         */
-        public function getByLocale($queryBuilder, $alias, $field, $data)
-        {
-            $active = true;
-            if (!$locale = $data['value']) {
-                $locale = $this->getDefaultLocale();
-                $active = false;
-            }
-            $queryBuilder->andWhere(sprintf('%s.locale = :locale', $alias));
-            $queryBuilder->orderBy(sprintf('%s.translationKey', $alias), 'asc');
-            $queryBuilder->setParameter(':locale', $locale);
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+            'locale',
+            'doctrine_orm_callback',
+            array(
+                'callback' => array(
+                    $this,
+                    'getByLocale'
+                )
+            ),
+            'choice',
+            array(
+                'empty_value' => false,
+                'choices' => $this->getLocaleChoices(),
+                'preferred_choices' => array($this->getDefaultLocale())
+            )
 
-            return $active;
+        );
+
+    }
+
+    /**
+     * @param \Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQuery $queryBuilder
+     * @param $alias
+     * @param $field
+     * @param $data
+     * @return bool
+     */
+    public function getByLocale($queryBuilder, $alias, $field, $data)
+    {
+        $active = true;
+        if (!$locale = $data['value']) {
+            $locale = $this->getDefaultLocale();
+            $active = false;
         }
+        $queryBuilder->andWhere(sprintf('%s.locale = :locale', $alias));
+        $queryBuilder->orderBy(sprintf('%s.translationKey', $alias), 'asc');
+        $queryBuilder->setParameter(':locale', $locale);
+
+        return $active;
+    }
 
 }
