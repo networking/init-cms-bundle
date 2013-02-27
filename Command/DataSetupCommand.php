c35499b5fb4b740813403b3ea76e0e065cd7f4e5
Sonja Brodersen <s.brodersen@networking.ch>
2013-02-27 17:04:45 +0100

    added commands: networking:initcms:install, networking:initcms:data-setup

---
+++ Command/DataSetupCommand.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+ * This file is part of the Networking package.
+ *
+ * (c) net working AG <info@networking.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Networking\InitCmsBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+class DataSetupCommand extends Command
+{
+    /**
+     * configuration for the command
+     */
+    protected function configure()
+    {
+        $this->setName('networking:initcms:data-setup')
+                ->setDescription('create and update db schema and append fixtures')
+                ->addOption('drop', '', InputOption::VALUE_NONE, 'If set: drop the existing db schema')
+                ->addOption('no-fixtures', '', InputOption::VALUE_NONE, 'If set: don\'t load fixtures')
+        ;
+
+    }
+
+    /**
+     * @param  \Symfony\Component\Console\Input\InputInterface   $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if($input->getOption('drop'))
+        {
+            $this->dropSchema($output);
+        }
+
+        $this->updateSchema($output);
+        $this->initACL($output);
+        $this->sonataSetupACL($output);
+
+        if(! $input->getOption('no-fixtures')){
+            $this->loadFixtures($output);
+        }
+    }
+
+    public function dropSchema(OutputInterface $output)
+    {
+        $command = $this->getApplication()->find('doctrine:schema:drop');
+
+        $arguments = array(
+            'command' => 'doctrine:schema:drop',
+            '--force' => true,
+        );
+
+        $input = new ArrayInput($arguments);
+
+        return $command->run($input, $output);
+    }
+
+    /**
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int|string
+     */
+    private function updateSchema(OutputInterface $output)
+    {
+        $command = $this->getApplication()->find('doctrine:schema:update');
+
+        $arguments = array(
+            'command' => 'doctrine:schema:update',
+            '--force' => true,
+        );
+
+        $input = new ArrayInput($arguments);
+
+        return $command->run($input, $output);
+    }
+
+    /**
+     * @param $output
+     * @return int
+     */
+    private function initACL($output)
+    {
+        $command = $this->getApplication()->find('init:acl');
+
+        $arguments = array(
+            'command' => 'init:acl'
+        );
+
+        $input = new ArrayInput($arguments);
+
+        return $command->run($input, $output);
+    }
+
+    /**
+     * @param $output
+     * @return int
+     */
+    private function sonataSetupACL($output)
+    {
+        $command = $this->getApplication()->find('sonata:admin:setup-acl');
+
+        $arguments = array(
+            'command' => 'sonata:admin:setup-acl'
+        );
+
+        $input = new ArrayInput($arguments);
+
+        return $command->run($input, $output);
+    }
+
+
+    /**
+     * interact
+     * unused at the moment
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @throws \Exception
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+    }
+
+
+    /**
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int|string
+     */
+    private function loadFixtures(OutputInterface $output)
+    {
+        $command = $this->getApplication()->find('doctrine:fixtures:load');
+
+        $arguments = array(
+            'command' => 'doctrine:fixtures:load',
+            '--fixtures' => __DIR__ . '/../Fixtures',
+            '--append' => true
+        );
+
+        $input = new ArrayInput($arguments);
+
+        return $command->run($input, $output);
+    }
+}
--- Command/InstallCommand.php
+++ Command/InstallCommand.php
@@ -16,6 +16,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class InstallCommand extends Command
 {
@@ -24,14 +25,11 @@ class InstallCommand extends Command
      */
     protected function configure()
     {
-        $this->setName('networking:cms:install')
-                ->setDescription('Install the Networking Init cms')
-//                ->setDefinition(
-//                    array(
-//                        new InputArgument('username', InputArgument::REQUIRED, 'The owner (username) of all automatically generated objects'),
-//                        new InputArgument('user_entity', InputArgument::REQUIRED, 'The Handler for users if unsure, try "ApplicationSonataUserBundle:User"')
-//                    )
-//                )
+        $this->setName('networking:initcms:install')
+                ->setDescription("Install the Networking Init cms: create update schema, load fixtures, create super user, dump assetic resources")
+                ->addOption('username', '', InputOption::VALUE_REQUIRED, 'username of the to be created super user')
+                ->addOption('email', '', InputOption::VALUE_REQUIRED, 'the email address of the to be created super user')
+                ->addOption('password', '', InputOption::VALUE_REQUIRED, 'password of the to be created super user')
         ;
 
     }
@@ -43,29 +41,18 @@ class InstallCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->createDB($output);
-        $this->initACL($output);
-        $this->sonataSetupACL($output);
-        $this->loadFixtures($output);
-        $this->createAdminUser($output);
+        $this->setupData($output);
+        $this->createAdminUser($input, $output);
         $this->dumpAssetic($output);
-
-//        $username   = $input->getArgument('username');
-//        $userEntity   = $input->getArgument('user_entity');
-//        $this->sonataGenerateObjectACL($output, $username, $userEntity);
     }
 
-    /**
-     * @param  \Symfony\Component\Console\Output\OutputInterface $output
-     * @return int|string
-     */
-    private function createDB(OutputInterface $output)
+    private function setupData($output)
     {
-        $command = $this->getApplication()->find('doctrine:schema:update');
+         $command = $this->getApplication()->find('networking:initcms:data-setup');
 
         $arguments = array(
-            'command' => 'doctrine:schema:update',
-            '--force' => true,
+            'command' => 'networking:initcms:data-setup',
+            '--drop' => true,
         );
 
         $input = new ArrayInput($arguments);
@@ -73,101 +60,6 @@ class InstallCommand extends Command
         return $command->run($input, $output);
     }
 
-    /**
-     * @param $output
-     * @return int
-     */
-    private function initACL($output)
-    {
-        $command = $this->getApplication()->find('init:acl');
-
-        $arguments = array(
-            'command' => 'init:acl'
-        );
-
-        $input = new ArrayInput($arguments);
-
-        return $command->run($input, $output);
-    }
-
-    /**
-     * @param $output
-     * @return int
-     */
-    private function sonataSetupACL($output)
-    {
-        $command = $this->getApplication()->find('sonata:admin:setup-acl');
-
-        $arguments = array(
-            'command' => 'sonata:admin:setup-acl'
-        );
-
-        $input = new ArrayInput($arguments);
-
-        return $command->run($input, $output);
-    }
-
-    /**
-     * unused at the moment
-     * @param $output
-     * @param $username
-     * @param string $bundle
-     * @return int
-     */
-    private function sonataGenerateObjectACL($output, $username, $bundle='NetworkingInitCmsBundle:User')
-    {
-        $command = $this->getApplication()->find('sonata:admin:generate-object-acl');
-
-        $arguments = array(
-            'command' => 'sonata:admin:generate-object-acl',
-            '--object_owner' => $username,
-            '--user_entity' => $bundle
-        );
-
-        $input = new ArrayInput($arguments);
-
-        return $command->run($input, $output);
-    }
-
-    /**
-     * interact
-     * unused at the moment
-     * @param \Symfony\Component\Console\Input\InputInterface $input
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     * @throws \Exception
-     */
-    protected function interact(InputInterface $input, OutputInterface $output)
-    {
-//        if (!$input->getArgument('username')) {
-//            $username = $this->getHelper('dialog')->askAndValidate(
-//                $output,
-//                'Please choose an existing username.'."\n".'He will be the owner of all automatically generated objects: ',
-//                function($username) {
-//                    if (empty($username)) {
-//                        throw new \Exception('Username can not be empty');
-//                    }
-//
-//                    return $username;
-//                }
-//            );
-//            $input->setArgument('username', $username);
-//        }
-//
-//        if (!$input->getArgument('user_entity')) {
-//            $userEntity = $this->getHelper('dialog')->askAndValidate(
-//                $output,
-//                'Please choose a user_entity (f.e. "ApplicationSonataUserBundle:User"): ',
-//                function($userEntity) {
-//                    if (empty($userEntity)) {
-//                        throw new \Exception('user_entity can not be empty');
-//                    }
-//
-//                    return $userEntity;
-//                }
-//            );
-//            $input->setArgument('user_entity', $userEntity);
-//        }
-    }
 
     /**
      * @param  \Symfony\Component\Console\Output\OutputInterface $output
@@ -190,38 +82,34 @@ class InstallCommand extends Command
     }
 
     /**
-     * @param  \Symfony\Component\Console\Output\OutputInterface $output
-     * @return int|string
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return mixed
      */
-    private function createAdminUser(OutputInterface $output)
+    private function createAdminUser(InputInterface $input, OutputInterface $output)
     {
+        $arguments = array();
+
+        $arguments['command'] = 'fos:user:create';
+
+        if($input->getOption('username')){
+            $arguments['username'] = $input->getOption('username');
+        }
+        if($input->getOption('email')){
+            $arguments['email'] = $input->getOption('email');
+        }
+        if($input->getOption('password')){
+            $arguments['password'] = $input->getOption('password');
+        }
         $command = $this->getApplication()->find('fos:user:create');
 
-        $arguments = array(
-            'command' => 'fos:user:create',
-            '--super-admin' => true
-        );
+        $arguments['--super-admin'] = true;
+
 
         $input = new ArrayInput($arguments);
 
         return $command->run($input, $output);
     }
 
-    /**
-     * @param  \Symfony\Component\Console\Output\OutputInterface $output
-     * @return int|string
-     */
-    private function loadFixtures(OutputInterface $output)
-    {
-        $command = $this->getApplication()->find('doctrine:fixtures:load');
 
-        $arguments = array(
-            'command' => 'doctrine:fixtures:load',
-            '--fixtures' => __DIR__ . '/../Fixtures'
-        );
-
-        $input = new ArrayInput($arguments);
-
-        return $command->run($input, $output);
-    }
 }
