cf6a0adfe814e2f11d617124acbf21a8bc842547
Yorkie Chadwick <y.chadwick@networking.ch>
2013-07-31 10:38:22 +0200

    added query to retrieve all menu items based on page publish status if applicable. Added path variable so that it can be set from the select statement

--- Entity/MenuItemRepository.php
+++ Entity/MenuItemRepository.php
@@ -10,32 +10,19 @@
  */
 namespace Networking\InitCmsBundle\Entity;
 
+use Doctrine\ORM\Query\Expr\Join;
 use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
+use Symfony\Component\Config\Definition\Builder\ExprBuilder;
 
 /**
- *  @author net working AG <info@networking.ch>
+ * @author net working AG <info@networking.ch>
  */
 class MenuItemRepository extends NestedTreeRepository
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function getNodesHierarchyQuery($node = null, $direct = false, array $options = array(), $includeNode = false)
-    {
-        return $this->getNodesHierarchyQueryBuilder($node, $direct, $options, $includeNode)->getQuery();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getNodesHierarchy($node = null, $direct = false, array $options = array(), $includeNode = false)
-    {
-        return $this->getNodesHierarchyQuery($node, $direct, $options, $includeNode)->getArrayResult();
-    }
 
     /**
      * @param $locale
-     * @param  null   $sortByField
+     * @param  null $sortByField
      * @param  string $direction
      * @return array
      */
@@ -44,8 +31,35 @@ class MenuItemRepository extends NestedTreeRepository
         $rootNodesQueryBuilder = $this->getRootNodesQueryBuilder($sortByField, $direction);
         $alias = $rootNodesQueryBuilder->getRootAliases();
         $rootNodesQueryBuilder->andWhere(sprintf('%s.locale = :locale', $alias[0]))
-                ->setParameter(':locale', $locale);
+            ->setParameter(':locale', $locale);
 
         return $rootNodesQueryBuilder->getQuery()->getResult();
+    }
+
+    public function getChildrenByStatus($node = null, $direct = false, $sortByField = null, $direction = 'ASC', $includeNode = false, $viewStatus = Page::STATUS_PUBLISHED)
+    {
+        $qb = $this->childrenQueryBuilder($node, $direct, $sortByField, $direction, $includeNode);
+        $aliases = $qb->getRootAliases();
+        $qb->leftJoin(sprintf('%s.page', $aliases[0]), 'p');
+        $qb->addSelect('cr.path AS path');
+        if ($viewStatus == Page::STATUS_PUBLISHED) {
+            $qb->leftJoin('p.snapshots', 'ps');
+            $qb->leftJoin('ps.contentRoute', 'cr');
+            $qb->andWhere($qb->expr()->orX('p.id = ps.page', 'p.id IS NULL'));
+        }else{
+            $qb->leftJoin('p.contentRoute', 'cr');
+        }
+        $qb->groupBy('p.id');
+
+        $results = $qb->getQuery()->getResult();
+        $menuItems = array();
+        foreach($results as $item){
+            $menuItem = $item[0];
+            $menuItem->setPath($item['path']);
+            $menuItems[] = $menuItem;
+        }
+
+
+        return $menuItems;
     }
 }
