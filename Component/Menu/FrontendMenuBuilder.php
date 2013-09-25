74f73e598912b37a08deb43f7cd7b7404955a324
Yorkie Chadwick <y.chadwick@networking.ch>
2013-09-25 16:14:38 +0200

    clean up menu builders

--- Component/Menu/FrontendMenuBuilder.php
+++ Component/Menu/FrontendMenuBuilder.php
@@ -14,11 +14,8 @@ namespace Networking\InitCmsBundle\Component\Menu;
 use Symfony\Component\HttpFoundation\Request,
     Symfony\Component\Security\Core\SecurityContextInterface,
     Symfony\Component\DependencyInjection\Container,
-    Knp\Menu\Iterator\RecursiveItemIterator,
     Networking\InitCmsBundle\Component\Menu\MenuBuilder,
     Networking\InitCmsBundle\Entity\MenuItem as Menu,
-    Networking\InitCmsBundle\Entity\MenuItemRepository,
-    Networking\InitCmsBundle\Component\Menu\MenuSubItemFilterIterator,
     Networking\InitCmsBundle\Entity\Page;
 
 /**
@@ -26,50 +23,28 @@ use Symfony\Component\HttpFoundation\Request,
  */
 class FrontendMenuBuilder extends MenuBuilder
 {
-
-
     /**
      * Creates the main page navigation for the left side of the top frontend navigation
      *
-     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param Request $request
      * @param $menuName
-     * @return \Knp\Menu\ItemInterface
+     * @param string $classes
+     * @return \Knp\Menu\ItemInterface|\Knp\Menu\MenuItem
      */
     public function createMainMenu(Request $request, $menuName, $classes = '')
     {
-
-        /** @var $repository MenuItemRepository */
-        $repository = $this->serviceContainer->get('doctrine')
-            ->getRepository('NetworkingInitCmsBundle:MenuItem');
         $menu = $this->createNavbarMenuItem();
         $menu->setChildrenAttribute('class', $classes);
         $menu->setCurrentUri($request->getRequestUri());
-
         /** @var $mainMenu Menu */
-        $mainMenu = $repository->findOneBy(
-            array('name' => $menuName, 'locale' => $this->serviceContainer->get('request')->getLocale())
-        );
+        $menuIterator = $this->getFullMenu($menuName);
 
-        if (!$mainMenu) {
+        if (!$menuIterator) {
             return $menu;
         }
 
-        $menuIterator = new \RecursiveIteratorIterator(
-            new RecursiveItemIterator($mainMenu->getChildrenByStatus($this->viewStatus)),
-            \RecursiveIteratorIterator::SELF_FIRST
-        );
-
-        foreach ($menuIterator as $childNode) {
-            if ($childNode->getVisibility() == Menu::VISIBILITY_PUBLIC || $this->isLoggedIn) {
-                if ($menuIterator->getDepth() > 0) {
-                    $parentMenu = $this->getMenuParentItem($menu, $childNode, $menuIterator->getDepth());
-                    $parentMenu->addChild($this->createFromNode($childNode));
-                } else {
-                    $menu->addChild($this->createFromNode($childNode));
-                }
-            }
-        }
-
+        $startDepth = 1;
+        $menu = $this->createMenu($menu, $menuIterator, $startDepth);
         return $menu;
     }
 
@@ -78,67 +53,30 @@ class FrontendMenuBuilder extends MenuBuilder
      * Create frontend sub navigation on the left hand side of the screen
      *
      * @param  \Symfony\Component\HttpFoundation\Request $request
-     * @param  string                                    $menuName
+     * @param  string $menuName
      * @param string $classes
      * @return bool|\Knp\Menu\ItemInterface
      */
     public function createSubnavMenu(Request $request, $menuName, $classes = 'nav nav-tabs nav-stacked')
     {
-        $root = null;
-        /** @var $repository MenuItemRepository */
-        $repository = $this->serviceContainer->get('doctrine')
-            ->getRepository('NetworkingInitCmsBundle:MenuItem');
-
-        $menu = $this->factory->createItem('sub_nav');
+        $menu = $this->createNavbarMenuItem();
+        $menu->setChildrenAttribute('class', $classes);
         $menu->setCurrentUri($request->getRequestUri());
 
         /** @var $mainMenu Menu */
-        $mainMenu = $repository->findOneBy(
-            array('name' => $menuName, 'locale' => $this->serviceContainer->get('request')->getLocale())
-        );
+        $menuIterator = $this->getSubMenu($menuName, 1);
 
-
-        if (!$mainMenu) {
+        if (!$menuIterator) {
             return $menu;
         }
 
-        /** @var $menuIterator \ArrayIterator */
-        $menuIterator = new \RecursiveIteratorIterator(
-            new RecursiveItemIterator($mainMenu->getChildren()),
-            \RecursiveIteratorIterator::SELF_FIRST
-        );
-
-        $iterator = new MenuSubItemFilterIterator($menuIterator, $request->getPathInfo());
-
-        foreach ($iterator as $item) {
-            $root = $item->getParentByLevel(1);
-        }
-
-        if (!$root) {
-            return false;
-        }
-
-        $menuIterator = new \RecursiveIteratorIterator(
-            new RecursiveItemIterator($root->getChildren()),
-            \RecursiveIteratorIterator::SELF_FIRST
-        );
-
-        $menu->setChildrenAttribute('class', $classes);
-
-        foreach ($menuIterator as $childNode) {
-            if ($childNode->getVisibility() == Menu::VISIBILITY_PUBLIC || $this->isLoggedIn) {
-                if ($menuIterator->getDepth() > 0) {
-                    $parentMenu = $this->getMenuParentItem($menu, $childNode, $menuIterator->getDepth());
-                    $parentMenu->addChild($this->createFromNode($childNode));
-                    $parentMenu->setChildrenAttribute('class', 'nav nav-list');
-                } else {
-                    $menu->addChild($this->createFromNode($childNode));
-                }
-            }
-        }
-
+        $startDepth = 2;
+        $menu = $this->createMenu($menu, $menuIterator, $startDepth);
+        $this->showOnlyCurrentChildren($menu);
+        $this->setRecursiveChildrenAttribute($menu, array('class' => 'nav nav-list'));
         return $menu;
     }
+
 
     /**
      * Creates the login and change language navigation for the right side of the top frontend navigation
@@ -147,12 +85,23 @@ class FrontendMenuBuilder extends MenuBuilder
      * @param string $classes
      * @return \Knp\Menu\ItemInterface
      */
+
+    /**
+     * Creates the login and change language navigation for the right side of the top frontend navigation
+     *
+     * @param Request $request
+     * @param $languages
+     * @param string $classes
+     * @param bool $dropDownMenu
+     * @return \Knp\Menu\ItemInterface
+     */
     public function createFrontendLangMenu(
         Request $request,
         $languages,
         $classes = 'nav pull-right',
         $dropDownMenu = false
-    ) {
+    )
+    {
         $menu = $this->factory->createItem('root');
         $menu->setChildrenAttribute('class', $classes);
 
@@ -175,32 +124,18 @@ class FrontendMenuBuilder extends MenuBuilder
      */
     public function createFooterMenu(Request $request, $menuName, $classes = '')
     {
-        $repository = $this->serviceContainer->get('doctrine')
-            ->getRepository('NetworkingInitCmsBundle:MenuItem');
         $menu = $this->createNavbarMenuItem();
         $menu->setChildrenAttribute('class', $classes);
         $menu->setCurrentUri($request->getRequestUri());
-        $mainMenu = $repository->findOneBy(
-            array('name' => $menuName, 'locale' => $this->serviceContainer->get('request')->getLocale())
-        );
-        if (!$mainMenu) {
+        /** @var $mainMenu Menu */
+        $menuIterator = $this->getFullMenu($menuName);
+
+        if (!$menuIterator) {
             return $menu;
         }
 
-        $menuIterator = new \RecursiveIteratorIterator(
-            new RecursiveItemIterator($mainMenu->getChildrenByStatus($this->viewStatus)),
-            \RecursiveIteratorIterator::SELF_FIRST
-        );
-
-        foreach ($menuIterator as $childNode) {
-            if ($menuIterator->getDepth() > 0) {
-                $parentMenu = $this->getMenuParentItem($menu, $childNode, $menuIterator->getDepth());
-                $parentMenu->addChild($this->createFromNode($childNode));
-            } else {
-                $menu->addChild($this->createFromNode($childNode));
-            }
-        }
-
+        $startDepth = 1;
+        $menu = $this->createMenu($menu, $menuIterator, $startDepth);
         return $menu;
     }
 
@@ -217,7 +152,8 @@ class FrontendMenuBuilder extends MenuBuilder
         array $languages,
         $currentLanguage,
         $route = 'networking_init_change_language'
-    ) {
+    )
+    {
         $this->addDivider($menu, true);
 
         $dropdown = $this->createDropdownMenuItem(
@@ -244,7 +180,8 @@ class FrontendMenuBuilder extends MenuBuilder
         array $languages,
         $currentLanguage,
         $route = 'networking_init_change_language'
-    ) {
+    )
+    {
         foreach ($languages as $language) {
             $node = $menu->addChild(
                 $language['label'],
