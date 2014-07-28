197eed2323619661cd15d3091712cea37b9b4155
Yorkie Chadwick <y.chadwick@networking.ch>
2014-07-28 09:29:07 +0200

    fix notice bar and other responsive layout changes

--- Resources/public/js/init_cms.js
+++ Resources/public/js/init_cms.js
@@ -1,5 +1,5 @@
 function createInitCmsMessageBox(status, message) {
-    var messageHtml = '<div class="col-md-offset-2 col-md-10 alert alert-' + status + '"><a class="close" data-dismiss="alert" href="#">×</a>' + message + '</div>';
+    var messageHtml = '<div class="col-sm-12 col-xs-12 col-md-offset-2 col-md-10 alert alert-' + status + '"><a class="close" data-dismiss="alert" href="#">×</a>' + message + '</div>';
 
     jQuery('.notice-block').html(messageHtml);
 }
