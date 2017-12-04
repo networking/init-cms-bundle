d338c3614e2a12b7edff4366fc15955a7a4f5589
Yorkie Chadwick <ychadwick@gmail.com>
2017-12-04 18:42:59 +0100

    added select2 extension

---
+++ Form/Extension/Select2Extension.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yorkie
+ * Date: 04.12.17
+ * Time: 13:34
+ */
+
+namespace Networking\InitCmsBundle\Form\Extension;
+
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Select2Extension extends AbstractTypeExtension {
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        if (true === $options['multiple'] && false === $options['expanded'] && isset($options['select2'])) {
+            $view->vars['select2'] = $options['select2'];
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefined('select2');
+    }
+
+    /**
+     * Returns the name of the type being extended.
+     *
+     * @return string The name of the type being extended
+     */
+    public function getExtendedType()
+    {
+        return ChoiceType::class;
+    }
+}
