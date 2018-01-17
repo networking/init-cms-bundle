e295bfcbc70eb26a9a0d65f15f0ec11e87c2d4a1
Yorkie Chadwick <ychadwick@gmail.com>
2018-01-17 10:33:15 +0100

    added method to stop cache rendering

--- src/Lib/PhpCache.php
+++ src/Lib/PhpCache.php
@@ -10,10 +10,12 @@
 
 namespace Networking\InitCmsBundle\Lib;
 umask(0002);
+
 use phpFastCache;
 use Symfony\Component\HttpFoundation\Request;
 
-class PhpCache implements PhpCacheInterface {
+class PhpCache implements PhpCacheInterface
+{
 
     /**
      * @var phpFastCache
@@ -50,18 +52,19 @@ class PhpCache implements PhpCacheInterface {
      * @param null $securityKey possibilities are auto (domain name), a string or null
      * @throws \Exception
      */
-    public function __construct($type = '', $rootDir = '', $env = '', $active = false, $cacheTime = '', $securityKey = null){
+    public function __construct($type = '', $rootDir = '', $env = '', $active = false, $cacheTime = '', $securityKey = null)
+    {
         $this->env = $env;
 
         $this->cacheDir = $this->createDir($rootDir, $env);
         $this->active = $active;
         $this->cacheTime = $cacheTime;
-        if(is_null($securityKey)){
-            $securityKey = 'cache.storage.'.sha1(__FILE__);
+        if (is_null($securityKey)) {
+            $securityKey = 'cache.storage.' . sha1(__FILE__);
         }
 
         phpFastCache::setup("storage", $type);
-        phpFastCache::setup("path", $this->cacheDir );
+        phpFastCache::setup("path", $this->cacheDir);
         phpFastCache::setup("securityKey", $securityKey);
         $this->phpFastCache = new phpFastCache($type);
     }
@@ -72,17 +75,18 @@ class PhpCache implements PhpCacheInterface {
      * @return string
      * @throws \Exception
      */
-    protected function createDir($rootDir, $env){
-        $cacheDir = $rootDir.sprintf('../var/cache/%s/php_fast_cache', $env);
-        if(!file_exists($cacheDir) || !is_writable($cacheDir)) {
-            if(!file_exists($cacheDir)) {
-                @mkdir($cacheDir,0777);
+    protected function createDir($rootDir, $env)
+    {
+        $cacheDir = $rootDir . sprintf('../var/cache/%s/php_fast_cache', $env);
+        if (!file_exists($cacheDir) || !is_writable($cacheDir)) {
+            if (!file_exists($cacheDir)) {
+                @mkdir($cacheDir, 0777);
             }
-            if(!is_writable($cacheDir)) {
-                @chmod($cacheDir,0777);
+            if (!is_writable($cacheDir)) {
+                @chmod($cacheDir, 0777);
             }
-            if(!file_exists($cacheDir) || !is_writable($cacheDir)) {
-                throw new \Exception("Sorry, Please create ".$cacheDir."/ and SET Mode 0777 or any Writable Permission!" , 100);
+            if (!file_exists($cacheDir) || !is_writable($cacheDir)) {
+                throw new \Exception("Sorry, Please create " . $cacheDir . "/ and SET Mode 0777 or any Writable Permission!", 100);
             }
         }
 
@@ -94,22 +98,29 @@ class PhpCache implements PhpCacheInterface {
      * @param $user
      * @return bool
      */
-    public function isCacheable(Request $request, $user){
+    public function isCacheable(Request $request, $user)
+    {
 
-        if($this->active == false)
-        {   return false;
-        }
-
-        if($this->env != 'prod'){
+        if (!$this->active) {
             return false;
         }
 
-        if($user)
-        {   return false;
+        if ($user) {
+            return false;
+        }
+
+        if ($request->getSession()->get('no_cache', false)) {
+            $request->getSession()->remove('no_cache');
+            return false;
         }
 
 
-        if($request->getMethod() != 'GET'){
+        if ($this->env != 'prod') {
+            return false;
+        }
+
+
+        if ($request->getMethod() != 'GET') {
             return false;
         }
 
@@ -121,7 +132,8 @@ class PhpCache implements PhpCacheInterface {
      * @param array $option
      * @return mixed|null|string
      */
-    public function get($keyword, $option = []){
+    public function get($keyword, $option = [])
+    {
         return $this->phpFastCache->instance->get($keyword, $option);
     }
 
@@ -133,8 +145,9 @@ class PhpCache implements PhpCacheInterface {
      * @param array $option
      * @return array|bool|string
      */
-    public function set($keyword, $value = "", $time = null, $option = []){
-        if(is_null($time)){
+    public function set($keyword, $value = "", $time = null, $option = [])
+    {
+        if (is_null($time)) {
             $time = $this->cacheTime;
         }
         return $this->phpFastCache->instance->set($keyword, $value, $time, $option);
@@ -145,7 +158,8 @@ class PhpCache implements PhpCacheInterface {
      * @param array $options
      * @return bool|\string[]
      */
-    public function delete($keyword, $options = []){
+    public function delete($keyword, $options = [])
+    {
         return $this->phpFastCache->instance->delete($keyword, $options);
     }
 
@@ -155,7 +169,7 @@ class PhpCache implements PhpCacheInterface {
      */
     public function clean($option = [])
     {
-        if($this->active){
+        if ($this->active) {
             return $this->phpFastCache->instance->clean($option);
         }
         return false;
@@ -167,8 +181,9 @@ class PhpCache implements PhpCacheInterface {
      * @param array $option
      * @return bool
      */
-    public function touch($keyword, $time = null, $option = []){
-        if(is_null($time)){
+    public function touch($keyword, $time = null, $option = [])
+    {
+        if (is_null($time)) {
             $time = $this->cacheTime;
         }
         return $this->phpFastCache->instance->touch($keyword, $time, $option);
@@ -179,7 +194,8 @@ class PhpCache implements PhpCacheInterface {
      * @param array $option
      * @return null
      */
-    public function getInfo($keyword, $option = []){
+    public function getInfo($keyword, $option = [])
+    {
         return $this->phpFastCache->instance->getInfo($keyword, $option);
     }
 
@@ -254,7 +270,6 @@ class PhpCache implements PhpCacheInterface {
     {
         $this->cacheTime = $cacheTime;
     }
-
 
 
 }
