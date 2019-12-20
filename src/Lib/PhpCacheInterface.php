d4567c1609bf64b6f14fd51ef0681ae9db26f52e
Yorkie Chadwick <ychadwick@gmail.com>
2019-12-20 13:19:44 +0100

    correct deprecation notice

--- src/Lib/PhpCacheInterface.php
+++ src/Lib/PhpCacheInterface.php
@@ -11,7 +11,7 @@ namespace Networking\InitCmsBundle\Lib;
 use Networking\InitCmsBundle\Cache\PageCacheInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-@trigger_error(sprintf('The "%s" interface is deprecated since InitCms 4.1, use "%s"  instead.', PhpCacheInterface::class,  PhpCacheInterface::class), E_USER_DEPRECATED);
+@trigger_error(sprintf('The "%s" interface is deprecated since InitCms 4.1, use "%s"  instead.', PhpCacheInterface::class,  PageCacheInterface::class), E_USER_DEPRECATED);
 
 /**
  * Interface PhpCacheInterface
