3a4e9d660a0ce4d685f1ebdae9d4cec321df5326
ychadwick <ychadwick>
2023-04-14 15:59:50 +0200

    fix media view action

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -10,7 +10,9 @@
 
 namespace Networking\InitCmsBundle\Controller;
 
+use Sonata\MediaBundle\Model\MediaManagerInterface;
 use Sonata\MediaBundle\Provider\MediaProviderInterface;
+use Sonata\MediaBundle\Provider\Pool;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,6 +22,12 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class MediaController extends AbstractController
 {
+    public function __construct(
+        private readonly MediaManagerInterface $mediaManager,
+        private readonly Pool $pool
+    )
+    {
+    }
     /**
      * output image direct to browser, retrieve from cache if activated.
      *
@@ -31,17 +39,32 @@ class MediaController extends AbstractController
      */
     public function viewImageAction(Request $request, $id, $format = MediaProviderInterface::FORMAT_REFERENCE)
     {
-        $media = $this->getMedia($id);
+        $media = $this->mediaManager->find($id);
 
         if (!$media) {
             throw new NotFoundHttpException(sprintf('unable to find the media with the id : %s', $id));
         }
 
-        if (!$this->container->get('sonata.media.pool')->getDownloadStrategy($media)->isGranted($media, $request)) {
+        if (!$this->pool->getDownloadStrategy($media)->isGranted($media, $request)) {
             throw new AccessDeniedException();
         }
-        $provider = $this->getProvider($media);
+        $provider = $this->pool->getProvider($media->getProviderName());
 
-        return new RedirectResponse($provider->generatePublicUrl($media, $format));
+        if ($format == 'reference') {
+            $file = $provider->getReferenceFile($media);
+        } else {
+            $file = $provider->getFilesystem()->get($provider->generatePrivateUrl($media, $format));
+        }
+
+
+        $content = $file->getContent();
+        $headers = array_merge(array(
+            'Content-Type' => $media->getContentType(),
+            'Accept-Ranges' => 'bytes',
+            'Content-Length' => $media->getSize(),
+            'Content-Disposition' => sprintf('inline; filename="%s"', $media->getMetadataValue('filename')),
+        ), array());
+
+       return new Response($content, 200, $headers);
     }
 }
