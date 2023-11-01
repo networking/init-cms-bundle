d55007719ade6200461c3b398ad7f93b3deadb50
ychadwick <ychadwick>
2023-11-01 08:44:30 +0100

    added return type

--- src/NetworkingInitCmsBundle.php
+++ src/NetworkingInitCmsBundle.php
@@ -25,7 +25,7 @@ use Networking\InitCmsBundle\DependencyInjection\Compiler\OverrideServiceCompile
  */
 class NetworkingInitCmsBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
 
