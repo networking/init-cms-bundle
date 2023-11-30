c8348a6e89cd82c9124df24881449b5c5fc5e043
ychadwick <ychadwick>
2023-11-30 13:30:21 +0100

    addition of security check command

---
+++ src/Command/SecurityCheckCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Networking\InitCmsBundle\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Formatter\OutputFormatter;
+use Symfony\Component\Console\Helper\Helper;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'networking:initcms:security-check',
+    description: 'Check installed PHP bundles, and installed NPM packages for security vulnerabilities.',
+)]
+class SecurityCheckCommand extends Command
+{
+    public function __construct(private readonly string $projectDir)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->title('Security Check');
+        $securityOutput = [];
+        $io->writeln('Symfony Security Check...');
+        $securityOutput[] = '';
+        exec('symfony security:check', $securityOutput, $return_var);
+
+        if(file_exists($this->projectDir.'/yarn.lock')){
+            $io->writeln('Running yarn audit...');
+            $securityOutput[] = '';
+            $securityOutput[] = sprintf('<comment>%s</>', 'Yarn Audit');
+            $securityOutput[] = sprintf('<comment>%s</>', '==========');
+            $securityOutput[] = '';
+            exec('yarn audit', $securityOutput, $return_var);
+        }
+
+        if(file_exists($this->projectDir.'/package-lock.json')){
+            $io->writeln('Running npm audit...');
+            $securityOutput[] = '';
+            $securityOutput[] = sprintf('<comment>%s</>', 'NPM Audit');
+            $securityOutput[] = sprintf('<comment>%s</>', '=========');
+            $securityOutput[] = '';
+            exec('npm audit --verbose', $securityOutput, $return_var);
+        }
+
+        foreach ($securityOutput as $line) {
+            $io->writeln($line);
+        }
+
+        if($return_var > 0){
+            $io->error('Security Check failed.');
+            return Command::FAILURE;
+        }
+
+
+        $io->success('Security Check completed.');
+
+
+        return Command::SUCCESS;
+    }
+}
