f8d09f65a17a71cc1f610e393869cd464b403797
Yorkie Chadwick <y.chadwick@networking.ch>
2024-02-26 17:09:08 +0100

    Refactor WebauthnCredential entities and model
    The code modifies the creation and initialization process of WebauthnCredential entities and associated model due to BC breaking changes in the symfony web-auth/webauthn-symfony-bundle. It adds new properties (backupEligible, backupStatus, uvInitialized), and a generate Unique ID method at persistence stage. The constructor method of the WebauthnCredentialModel has been restructured as well.

--- src/Entity/WebauthnCredential.php
+++ src/Entity/WebauthnCredential.php
@@ -1,27 +1,43 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Networking\InitCmsBundle\Entity;
 
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\PrePersist;
+use Doctrine\ORM\Mapping\Table;
 use Networking\InitCmsBundle\Model\WebauthnCredentialModel;
 use Networking\InitCmsBundle\Repository\WebauthnCredentialRepository;
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Column;
-use Doctrine\ORM\Mapping\GeneratedValue;
-use Doctrine\ORM\Mapping\Id;
-use Doctrine\ORM\Mapping\Table;
+use Symfony\Component\Uid\Ulid;
 
-
-#[Table(name: "webauthn_credentials")]
+#[Table(name: 'webauthn_credentials')]
 #[Entity(repositoryClass: WebauthnCredentialRepository::class)]
+#[HasLifecycleCallbacks]
 class WebauthnCredential extends WebauthnCredentialModel
 {
     #[Id]
     #[Column(unique: true)]
-    #[GeneratedValue(strategy: "NONE")]
-    protected string $id;
+    #[GeneratedValue(strategy: 'NONE')]
+    protected ?string $id = null;
 
+    public ?bool $backupEligible = null;
 
+    public ?bool $backupStatus = null;
+
+    public ?bool $uvInitialized = null;
+
+    #[PrePersist]
+    public function prePersist(): void
+    {
+        if (null === $this->id) {
+            $this->id = Ulid::generate();
+        }
+    }
 
     public function getId(): string
     {
--- src/Model/WebauthnCredentialModel.php
+++ src/Model/WebauthnCredentialModel.php
@@ -2,44 +2,14 @@
 
 namespace Networking\InitCmsBundle\Model;
 
-use Symfony\Component\Uid\AbstractUid;
-use Symfony\Component\Uid\Ulid;
 use Webauthn\PublicKeyCredentialSource;
-use Webauthn\TrustPath\TrustPath;
 
-if(class_exists(PublicKeyCredentialSource::class)){
+if (class_exists(PublicKeyCredentialSource::class)) {
     class WebauthnCredentialModel extends PublicKeyCredentialSource
     {
-        public function __construct(
-            string $publicKeyCredentialId,
-            string $type,
-            array $transports,
-            string $attestationType,
-            TrustPath $trustPath,
-            AbstractUid $aaguid,
-            string $credentialPublicKey,
-            string $userHandle,
-            int $counter,
-            ?array $otherUi
-        ) {
-            $this->id = Ulid::generate();
-            parent::__construct(
-                $publicKeyCredentialId,
-                $type,
-                $transports,
-                $attestationType,
-                $trustPath,
-                $aaguid,
-                $credentialPublicKey,
-                $userHandle,
-                $counter,
-                $otherUi
-            );
-        }
     }
-}else {
+} else {
     class WebauthnCredentialModel
     {
-
     }
 }
