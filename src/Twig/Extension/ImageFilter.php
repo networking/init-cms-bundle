8f070ff7b1911c32b354b12f14f84f4c84bf2ead
Yorkie Chadwick <y.chadwick@networking.ch>
2024-06-13 16:05:58 +0200

    Update image suffix handling in ImageFilter extension
    The ImageFilter extension has been updated to correctly handle image suffixes, accommodating for variations in height, width, or both. A try-catch block has been added to handle exceptions when creating directories, improving the stability of the code.
    
    Signed-off-by: Yorkie Chadwick <y.chadwick@networking.ch>

--- src/Twig/Extension/ImageFilter.php
+++ src/Twig/Extension/ImageFilter.php
@@ -4,61 +4,75 @@ declare(strict_types=1);
 
 namespace Networking\InitCmsBundle\Twig\Extension;
 
-use League\Flysystem\Config;
 use League\Flysystem\Filesystem;
 use Twig\TwigFilter;
 
-class ImageFilter extends \Twig\Extension\AbstractExtension{
-
+class ImageFilter extends \Twig\Extension\AbstractExtension
+{
     public function __construct(private readonly Filesystem $flysystem, private readonly Filesystem $cacheFlysystem)
     {
     }
+
     public function getFilters(): array
     {
         return [
-            new TwigFilter('webp_image', $this->webpImage(...))
+            new TwigFilter('webp_image', $this->webpImage(...)),
         ];
     }
 
     public function webpImage(string $imagePath, ?int $width = null, ?int $height = null, $compression = 80): string
     {
         $mimeType = $this->flysystem->mimeType(urldecode($imagePath));
-        if($mimeType === 'image/svg' || $mimeType === 'image/svg+xml'){
+        if ('image/svg' === $mimeType || 'image/svg+xml' === $mimeType) {
             return $imagePath;
         }
 
-        $newImagePath = str_replace(['.jpeg','.jpg', '.png', '.gif'], '.webp', $imagePath);
+        $suffix = '.webp';
 
-        if($this->cacheFlysystem->has($newImagePath)){
-            return '/media/cache'.$newImagePath ;
+        if (null !== $height && null !== $width) {
+            $suffix = '-'.$width.'x'.$height.$suffix;
+        }
+
+        if (null !== $width && null === $height) {
+            $suffix = '-'.$width.'w'.$suffix;
+        }
+
+        if (null !== $height && null === $width) {
+            $suffix = '-'.$height.'h'.$suffix;
+        }
+
+        $newImagePath = str_replace(['.jpeg', '.jpg', '.png', '.gif'], $suffix, $imagePath);
+
+        if ($this->cacheFlysystem->has($newImagePath)) {
+            return '/media/cache'.$newImagePath;
         }
 
         $parts = explode('/', $newImagePath);
 
         $dir = str_replace(end($parts), '', $newImagePath);
 
-        if(!$this->cacheFlysystem->directoryExists($dir)){
-            $dirCreated = $this->cacheFlysystem->createDirectory($dir, ['visibility' => 'public'] );
-            if(!$dirCreated){
+        if (!$this->cacheFlysystem->directoryExists($dir)) {
+            try {
+                $this->cacheFlysystem->createDirectory($dir, ['visibility' => 'public']);
+            } catch (\Exception) {
                 return $imagePath;
             }
         }
 
-        try{
-            if(class_exists(\Imagick::class)){
+        try {
+            if (class_exists(\Imagick::class)) {
                 $image = $this->createWithImagick($imagePath, $width, $height, $compression);
-            }else{
+            } else {
                 $image = $this->createWithGD($imagePath, $width, $height);
             }
 
             $this->cacheFlysystem->write($newImagePath, $image, ['visibility' => 'public']);
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             return $imagePath;
         }
 
         return '/media/cache'.$newImagePath;
     }
-
 
     public function createWithGD($imagePath, $width, $height): string|false
     {
@@ -67,8 +81,8 @@ class ImageFilter extends \Twig\Extension\AbstractExtension{
         imagepalettetotruecolor($gd);
 
         ob_start();
-        if($width && $height){
-            $gd = imagescale($gd,$width, $height);
+        if ($width && $height) {
+            $gd = imagescale($gd, $width, $height);
         }
         imagewebp($gd, null, IMG_WEBP_LOSSLESS);
         $gd = ob_get_contents();
@@ -81,10 +95,10 @@ class ImageFilter extends \Twig\Extension\AbstractExtension{
     {
         $im = new \Imagick();
         $im->readImageFile($this->flysystem->readStream($imagePath));
-        if($width && $height){
+        if ($width && $height) {
             $im->resizeImage($width, $height, \Imagick::FILTER_UNDEFINED, 1);
         }
-        $im->setImageFormat( "webp" );
+        $im->setImageFormat('webp');
         $im->setOption('webp:method', '6');
         $im->setImageCompressionQuality($compression);
 
