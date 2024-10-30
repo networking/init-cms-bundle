512354a2576b9463fba1974ff72ce93540d2274f
Yorkie Chadwick <y.chadwick@networking.ch>
2024-10-30 13:20:12 +0100

    Refactor CheckEmailAction constructor for better type handling
    Revised the constructor in CheckEmailAction to improve type checks and handle deprecations. This change includes conditional checks based on the types of arguments passed, with clear handling of deprecated types. The update ensures more robust type safety and paves the way for future simplifications.

--- src/Action/CheckEmailAction.php
+++ src/Action/CheckEmailAction.php
@@ -23,13 +23,88 @@ use Twig\Environment;
 
 final class CheckEmailAction
 {
+    private Pool $adminPool;
+    private TemplateRegistryInterface $templateRegistry;
+    private int $tokenTtl;
+
+    /**
+     * NEXT_MAJOR: Remove `$tokenTtlDeprecated` argument and only allow first types of arguments.
+     */
     public function __construct(
         private Environment $twig,
-        private UrlGeneratorInterface $urlGenerator,
-        private Pool $adminPool,
-        private TemplateRegistryInterface $templateRegistry,
-        private int $tokenTtl
+        Pool|UrlGeneratorInterface $adminPool,
+        TemplateRegistryInterface|Pool $templateRegistry,
+        int|TemplateRegistryInterface $tokenTtl,
+        ?int $tokenTtlDeprecated = null,
     ) {
+        // NEXT_MAJOR: Remove all checks and use constructor property promotion instead
+        if ($adminPool instanceof UrlGeneratorInterface) {
+            if (!$templateRegistry instanceof Pool) {
+                throw new \TypeError(\sprintf(
+                    'Argument 3 passed to %s() must be an instance of %s, %s given.',
+                    __METHOD__,
+                    Pool::class,
+                    $templateRegistry::class
+                ));
+            }
+            $this->adminPool = $templateRegistry;
+
+            if (!$tokenTtl instanceof TemplateRegistryInterface) {
+                throw new \TypeError(\sprintf(
+                    'Argument 4 passed to %s() must be an instance of %s, int given.',
+                    __METHOD__,
+                    TemplateRegistryInterface::class,
+                ));
+            }
+            $this->templateRegistry = $tokenTtl;
+
+            if (!\is_int($tokenTtlDeprecated)) {
+                throw new \TypeError(\sprintf(
+                    'Argument 5 passed to %s() must be type of %s, %s given.',
+                    __METHOD__,
+                    'integer',
+                    \gettype($tokenTtlDeprecated)
+                ));
+            }
+            $this->tokenTtl = $tokenTtlDeprecated;
+
+            @trigger_error(\sprintf(
+                'Passing an instance of %s as argument 2 to "%s()" is deprecated since sonata-project/user-bundle 5.13 and will only accept an instance of %s in version 6.0.',
+                UrlGeneratorInterface::class,
+                __METHOD__,
+                Pool::class
+            ), \E_USER_DEPRECATED);
+        } else {
+            $this->adminPool = $adminPool;
+            if (!$templateRegistry instanceof TemplateRegistryInterface) {
+                throw new \TypeError(\sprintf(
+                    'Argument 3 passed to %s() must be an instance of %s, %s given.',
+                    __METHOD__,
+                    TemplateRegistryInterface::class,
+                    $templateRegistry::class
+                ));
+            }
+            $this->templateRegistry = $templateRegistry;
+
+            if (!\is_int($tokenTtl)) {
+                throw new \TypeError(\sprintf(
+                    'Argument 4 passed to %s() must be type of %s, %s given.',
+                    __METHOD__,
+                    'integer',
+                    \gettype($tokenTtl)
+                ));
+            }
+            $this->tokenTtl = $tokenTtl;
+
+            if (null !== $tokenTtlDeprecated) {
+                throw new \TypeError(\sprintf(
+                    'Argument 5 passed to %s() must be %s, %s given.',
+                    __METHOD__,
+                    'NULL',
+                    \gettype($tokenTtlDeprecated)
+                ));
+            }
+        }
     }
 
     public function __invoke(Request $request): Response
